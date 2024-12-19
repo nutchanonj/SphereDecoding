bb4e0d38cf91806ffa485e7dfb7314d0e4b4ce54
Nutchanon J <nutchanon.non@outlook.co.th>
2024-12-20 06:40:41 +0800

    add files about testing

--- H_generator.m
+++ H_generator.m
@@ -4,22 +4,27 @@ clear
 
 fileID = fopen('data_I.dat','w');
 rng(1)
-bit_sent = randi([0 1],1,12*11);
-bit_received = zeros(1,12*11);
+H_num = 500;
+bit_sent = randi([0 1],1,12*11*H_num);
+bit_received_ML = zeros(1,12*11*H_num);
+bit_received_DFS = zeros(1,12*11*H_num);
+bit_received_DFS_fix = zeros(1,12*11*H_num);
+bit_numbers = 10;
+snr = 15;
 
-for p = 1:1
+for p = 1:H_num
     rng(p)
     H = normrnd(0,0.5,[4,4]) + 1i*normrnd(0,0.5,[4,4]);
     [Q,R] = qr(H);
-    Q_shift = Q;%round(Q*(2^0));
-    R_shift = R;%round(R*(2^0));
+    Q_shift = round(Q*(2^bit_numbers));
+    R_shift = round(R*(2^bit_numbers));
     for i = 1:4
         fprintf(fileID,"1");
         for j = 1:4
-            Rij = fi(real(R_shift(i,j)),1,16,10,'RoundingMethod','Nearest');
+            Rij = fi(real(R(i,j)),1,bit_numbers+6,bit_numbers,'RoundingMethod','Nearest');
             Rij = bin(Rij);
             fprintf(fileID,"%s", Rij);
-            Rij = fi(imag(R_shift(i,j)),1,16,10,'RoundingMethod','Nearest');
+            Rij = fi(imag(R(i,j)),1,bit_numbers+6,bit_numbers,'RoundingMethod','Nearest');
             Rij = bin(Rij);
             fprintf(fileID,"%s", Rij);
         end
@@ -51,36 +56,211 @@ for p = 1:1
         x_sent = code(x_sent_num+1).';
         y_sent = H*x_sent;
         sigpower = pow2db(mean(abs(x_sent).^2));
-        snr = 19;
         y_received = awgn(y_sent,snr,sigpower,p);
     
         y_received_R = Q' * y_received;
-        y_received_R_shift = y_received_R;%round(y_received_R*(2^16));
+        y_received_R_shift = round(y_received_R*(2^bit_numbers));
         fprintf(fileID,"0");
         for i = 1:4
-            In_0_real = fi(real(y_received_R_shift(i)),1,16,10,'RoundingMethod','Nearest');
+            In_0_real = fi(real(y_received_R(i)),1,bit_numbers+6,bit_numbers,'RoundingMethod','Nearest');
             In_0_real = bin(In_0_real);
             fprintf(fileID,"%s", In_0_real);
-            In_0_imag = fi(imag(y_received_R_shift(i)),1,16,10,'RoundingMethod','Nearest');
+            In_0_imag = fi(imag(y_received_R(i)),1,bit_numbers+6,bit_numbers,'RoundingMethod','Nearest');
             In_0_imag = bin(In_0_imag);
             fprintf(fileID,"%s", In_0_imag);
         end
         fprintf(fileID,"\n");
 
+        % For calculating ML detection.
         gamma = sum(abs(y_received - H*x).^2,1);
         [gamma_min,gamma_min_index] = min(gamma);
         x_ML = x(:,gamma_min_index);
-        x_ML_num = psk8decode(x_ML)
+        x_ML_num = psk8decode(x_ML);
 
         for r = 1:4
             x_ML_bit = psk8decode_bit(x_ML_num(r));
-            bit_received(1,132*(p-1)+12*(q-1)+3*(r-1)+1:132*(p-1)+12*(q-1)+3*r) = x_ML_bit;
+            bit_received_ML(1,132*(p-1)+12*(q-1)+3*(r-1)+1:132*(p-1)+12*(q-1)+3*r) = x_ML_bit;
+        end
+
+        % For calculating DFS l2 Sphere detection (floating point)
+
+        go_deeper = true;
+        cost_best = 10000000000000000000000000000000000000000000000000;
+        current_x_DFS = zeros(4,1);
+        best_x_DFS = zeros(4,1);
+        cost_T4_real = 0;
+        cost_T4_imag = 0;
+        cost_T4_abs  = 0;
+        cost_T3_real = 0;
+        cost_T3_imag = 0;
+        cost_T3_abs  = 0;
+        cost_T2_real = 0;
+        cost_T2_imag = 0;
+        cost_T2_abs  = 0;
+        cost_T1_real = 0;
+        cost_T1_imag = 0;
+        cost_T1_abs  = 0;
+        cost_accum   = 0;
+
+        for i = 1:8
+            current_x_DFS(4) = code(i);
+            % calculate cost
+            R9S3 = R(4,4)*current_x_DFS(4);
+            cost_T4_real = real(y_received_R(4)) - real(R9S3);
+            cost_T4_imag = imag(y_received_R(4)) - imag(R9S3);
+            cost_T4_abs  = cost_T4_real^2 + cost_T4_imag^2;
+            cost_accum = cost_T4_abs;
+            if cost_accum >= cost_best
+                continue
+            end
+            for j = 1:8
+                current_x_DFS(3) = code(j);
+                % calculate cost
+                R7S2 = R(3,3)*current_x_DFS(3);
+                R8S3 = R(3,4)*current_x_DFS(4);
+                cost_T3_real = real(y_received_R(3)) - real(R7S2) - real(R8S3);
+                cost_T3_imag = imag(y_received_R(3)) - imag(R7S2) - imag(R8S3);
+                cost_T3_abs  = cost_T3_real^2 + cost_T3_imag^2;
+                cost_accum = cost_T4_abs + cost_T3_abs;
+                if cost_accum >= cost_best
+                    continue
+                end
+                for k = 1:8
+                    current_x_DFS(2) = code(k);
+                    % calculate cost
+                    R4S1 = R(2,2)*current_x_DFS(2);
+                    R5S2 = R(2,3)*current_x_DFS(3);
+                    R6S3 = R(2,4)*current_x_DFS(4);
+                    cost_T2_real = real(y_received_R(2)) - real(R4S1) - real(R5S2) - real(R6S3);
+                    cost_T2_imag = imag(y_received_R(2)) - imag(R4S1) - imag(R5S2) - imag(R6S3);
+                    cost_T2_abs  = cost_T2_real^2 + cost_T2_imag^2;
+                    cost_accum = cost_T4_abs + cost_T3_abs + cost_T2_abs;
+                    if cost_accum >= cost_best
+                        continue
+                    end
+                    for l = 1:8
+                        current_x_DFS(1) = code(l);
+                        % calculate cost
+                        R0S0 = R(1,1)*current_x_DFS(1);
+                        R1S1 = R(1,2)*current_x_DFS(2);
+                        R2S2 = R(1,3)*current_x_DFS(3);
+                        R3S3 = R(1,4)*current_x_DFS(4);
+                        cost_T1_real = real(y_received_R(1)) - real(R0S0) - real(R1S1) - real(R2S2) - real(R3S3);
+                        cost_T1_imag = imag(y_received_R(1)) - imag(R0S0) - imag(R1S1) - imag(R2S2) - imag(R3S3);
+                        cost_T1_abs  = cost_T1_real^2 + cost_T1_imag^2;
+                        cost_accum = cost_T4_abs + cost_T3_abs + cost_T2_abs + cost_T1_abs;
+                        if cost_accum < cost_best
+                            cost_best = cost_accum;
+                            best_x_DFS = current_x_DFS;
+                        end
+                    end
+                end
+            end
+        end
+
+        x_DFS_num = psk8decode(best_x_DFS);
+
+        for r = 1:4
+            x_DFS_bit = psk8decode_bit(x_DFS_num(r));
+            bit_received_DFS(1,132*(p-1)+12*(q-1)+3*(r-1)+1:132*(p-1)+12*(q-1)+3*r) = x_DFS_bit;
+        end
+
+        % For calculating DFS l2 Sphere detection (fixed point)
+        code = [1, (181/256)+(181/256)*1i, 1i, -(181/256)+(181/256)*1i, -1, -(181/256)-(181/256)*1i, -1i, (181/256)-(181/256)*1i];
+
+        go_deeper = true;
+        cost_best = 10000000000000000000000000000000000000000000000000;
+        current_x_DFS = zeros(4,1);
+        best_x_DFS = zeros(4,1);
+        cost_T4_real = 0;
+        cost_T4_imag = 0;
+        cost_T4_abs  = 0;
+        cost_T3_real = 0;
+        cost_T3_imag = 0;
+        cost_T3_abs  = 0;
+        cost_T2_real = 0;
+        cost_T2_imag = 0;
+        cost_T2_abs  = 0;
+        cost_T1_real = 0;
+        cost_T1_imag = 0;
+        cost_T1_abs  = 0;
+        cost_accum   = 0;
+
+        for i = 1:8
+            current_x_DFS(4) = code(i);
+            % calculate cost
+            R9S3 = floor(R_shift(4,4)*current_x_DFS(4));
+            cost_T4_real = real(y_received_R_shift(4)) - real(R9S3);
+            cost_T4_imag = imag(y_received_R_shift(4)) - imag(R9S3);
+            cost_T4_abs  = cost_T4_real^2 + cost_T4_imag^2;
+            cost_accum = floor((cost_T4_abs)/1024);
+            if cost_accum >= cost_best
+                continue
+            end
+            for j = 1:8
+                current_x_DFS(3) = code(j);
+                % calculate cost
+                R7S2 = floor(R_shift(3,3)*current_x_DFS(3));
+                R8S3 = floor(R_shift(3,4)*current_x_DFS(4));
+                cost_T3_real = real(y_received_R_shift(3)) - real(R7S2) - real(R8S3);
+                cost_T3_imag = imag(y_received_R_shift(3)) - imag(R7S2) - imag(R8S3);
+                cost_T3_abs  = cost_T3_real^2 + cost_T3_imag^2;
+                cost_accum = floor((cost_T4_abs + cost_T3_abs)/1024);
+                if cost_accum >= cost_best
+                    continue
+                end
+                for k = 1:8
+                    current_x_DFS(2) = code(k);
+                    % calculate cost
+                    R4S1 = floor(R_shift(2,2)*current_x_DFS(2));
+                    R5S2 = floor(R_shift(2,3)*current_x_DFS(3));
+                    R6S3 = floor(R_shift(2,4)*current_x_DFS(4));
+                    cost_T2_real = real(y_received_R_shift(2)) - real(R4S1) - real(R5S2) - real(R6S3);
+                    cost_T2_imag = imag(y_received_R_shift(2)) - imag(R4S1) - imag(R5S2) - imag(R6S3);
+                    cost_T2_abs  = cost_T2_real^2 + cost_T2_imag^2;
+                    cost_accum = floor((cost_T4_abs + cost_T3_abs + cost_T2_abs)/1024);
+                    if cost_accum >= cost_best
+                        continue
+                    end
+                    for l = 1:8
+                        current_x_DFS(1) = code(l);
+                        % calculate cost
+                        R0S0 = floor(R_shift(1,1)*current_x_DFS(1));
+                        R1S1 = floor(R_shift(1,2)*current_x_DFS(2));
+                        R2S2 = floor(R_shift(1,3)*current_x_DFS(3));
+                        R3S3 = floor(R_shift(1,4)*current_x_DFS(4));
+                        cost_T1_real = real(y_received_R_shift(1)) - real(R0S0) - real(R1S1) - real(R2S2) - real(R3S3);
+                        cost_T1_imag = imag(y_received_R_shift(1)) - imag(R0S0) - imag(R1S1) - imag(R2S2) - imag(R3S3);
+                        cost_T1_abs  = cost_T1_real^2 + cost_T1_imag^2;
+                        cost_accum = floor((cost_T4_abs + cost_T3_abs + cost_T2_abs + cost_T1_abs)/1024);
+                        if cost_accum < cost_best
+                            cost_best = cost_accum;
+                            best_x_DFS = current_x_DFS;
+                        end
+                    end
+                end
+            end
+        end
+
+        x_DFS_fix_num = psk8decode(best_x_DFS);
+
+        for r = 1:4
+            x_DFS_fix_bit = psk8decode_bit(x_DFS_fix_num(r));
+            bit_received_DFS_fix(1,132*(p-1)+12*(q-1)+3*(r-1)+1:132*(p-1)+12*(q-1)+3*r) = x_DFS_fix_bit;
         end
 
     end
+
 end
-sum(abs( bit_received - bit_sent ))
-BER_ML = sum(abs( bit_received - bit_sent ))/66000;
+
+BER_ML = sum(abs( bit_received_ML - bit_sent ))/(12*11*H_num);
+BER_DFS = sum(abs( bit_received_DFS - bit_sent ))/(12*11*H_num);
+BER_DFS_fix = sum(abs( bit_received_DFS_fix - bit_sent ))/(12*11*H_num);
+
+format longg
+disp(BER_ML)
+disp(BER_DFS)
+disp(BER_DFS_fix)
 
 %figure(1)
 %plot(gamma)
@@ -92,42 +272,44 @@ BER_ML = sum(abs( bit_received - bit_sent ))/66000;
 
 %% second section.
 
-% bit_received_verilog = zeros(1,11*12);
-% % Open the file
-% fileID = fopen('Data_Out.txt', 'r');
-% 
-% % Check if the file opens successfully
-% if fileID == -1
-%     error('File cannot be opened. Check file path.');
-% end
-% 
-% % Initialize an empty cell array to store each line
-% binary1DVector = zeros(1,11*12);
-% 
-% % Read the file line-by-line
-% lineIndex = 1;
-% while ~feof(fileID)
-%     line = fgetl(fileID); % Read one line as a string
-%     if ischar(line)       % Check if the line is valid
-%         binary1DVector = strcat(binary1DVector, line); % Concatenate each line
-%     end
-% end
-% 
-% for i = 1:11
-%     for j = 1:3
-%         bit_received_verilog((i-1)*12 + j ) = binary1DVector((i-1)*12 + j + 9 ) - '0';
-%     end
-%     for j = 4:6
-%         bit_received_verilog((i-1)*12 + j ) = binary1DVector((i-1)*12 + j + 3) - '0';
-%     end
-%     for j = 7:9
-%         bit_received_verilog((i-1)*12 + j ) = binary1DVector((i-1)*12 + j - 3) - '0';
-%     end
-%     for j = 10:12
-%         bit_received_verilog((i-1)*12 + j ) = binary1DVector((i-1)*12 + j -9 ) - '0';
-%     end
-% end
-% sum(abs( bit_received_verilog - bit_sent ))
-% BER_verilog = sum(abs( bit_received_verilog - bit_sent ))/66000;
-% BER_verilog
-% 
+bit_received_verilog = zeros(1,11*12*H_num);
+% Open the file
+fileID = fopen('Data_Out_SNR18.txt', 'r');
+
+% Check if the file opens successfully
+if fileID == -1
+    error('File cannot be opened. Check file path.');
+end
+
+% Initialize an empty cell array to store each line
+binary1DVector = zeros(1,11*12*H_num);
+
+% Read the file line-by-line
+lineIndex = 1;
+while ~feof(fileID)
+    line = fgetl(fileID); % Read one line as a string
+    if ischar(line)       % Check if the line is valid
+        binary1DVector = strcat(binary1DVector, line); % Concatenate each line
+    end
+end
+
+for p = 1:H_num
+    for i = 1:11
+        for j = 1:3
+            bit_received_verilog((p-1)*132 + (i-1)*12 + j ) = binary1DVector((p-1)*132 + (i-1)*12 + j + 9) - '0';
+        end
+        for j = 4:6
+            bit_received_verilog((p-1)*132 + (i-1)*12 + j ) = binary1DVector((p-1)*132 + (i-1)*12 + j + 3) - '0';
+        end
+        for j = 7:9
+            bit_received_verilog((p-1)*132 + (i-1)*12 + j ) = binary1DVector((p-1)*132 + (i-1)*12 + j - 3) - '0';
+        end
+        for j = 10:12
+            bit_received_verilog((p-1)*132 + (i-1)*12 + j ) = binary1DVector((p-1)*132 + (i-1)*12 + j -9 ) - '0';
+        end
+    end
+end
+
+BER_verilog = sum(abs( bit_received_verilog - bit_sent ))/(11*12*H_num);
+disp(BER_verilog)
+
---
+++ top_tb_NutchanonJ.v
@@ -0,0 +1,224 @@
+/********************************************************************
+* Filename: testbench_mod_add.v
+* Authors:
+*     Khoa Tran
+* Description:
+*     testbench for modular addition/substraction
+* Parameters:
+*
+* Note:
+*
+* Review History:
+*     2024.12.05             Khoa Tran
+*********************************************************************/
+
+`timescale 1ns/10ps
+`define PERIOD    8.1
+`define MAX_CYCLE 100000
+`define RST_DELAY 2.0
+`define I_DELAY   4.2
+`define IDATA  "data_I.dat"
+
+`define PAT_LEN 15 
+`define H_LEN 1
+
+
+module top_tb  #(
+    parameter INST_W = 1,
+    parameter INT_W  = 6,
+    parameter FRAC_W = 10,
+    parameter I_WIDTH = INT_W + FRAC_W
+) ();
+
+    // Ports
+    wire              clk;
+    wire              rst;
+    wire              rst_n;
+    reg               in_valid;
+    reg  [0:0]        inst;
+    reg  [(I_WIDTH*4*2)-1:0]     idata_a;
+
+
+    wire              busy;
+    wire              out_valid;
+	wire              out_in_ready;
+    wire [(12)-1:0]   odata;
+
+    // TB variables
+    reg  [(I_WIDTH*4*2)  :0]    input_data  [0:(`PAT_LEN*`H_LEN)-1];
+    //reg  [(I_WIDTH*4*2)-1:0]    golden_data [0:`PAT_LEN-1];
+
+    integer input_end, output_end, test_end;
+    integer j, k, H_num;
+    integer correct, error, cycle_count;
+	integer FILE;
+    initial begin
+        $readmemb(`IDATA, input_data);
+		FILE = $fopen("Data_Out.txt");
+        //$readmemb(`ODATA, golden_data);
+    end
+
+    
+    clk_gen u_clk_gen (
+        .clk   (clk  ),
+        .rst   (rst  ),
+        .rst_n (rst_n),
+        .k     (k    )
+    );
+	
+	MIMO_detector MIMO_detector_inst (
+		.Clk              (clk),
+		.Reset            (rst),
+		.i_in_valid       (in_valid),
+		.flagChannelorData(inst),
+		.InData           (idata_a),
+		.OutData          (odata),
+		.o_in_ready       (out_in_ready),
+		.OutputReady      (out_valid)
+	);
+
+    // Input
+    initial begin
+        input_end = 0;
+
+        // reset
+        wait (rst === 1'b1);
+        in_valid =  1'b0;
+        idata_a  = 255'd0;
+        wait (rst === 1'b0);
+
+        // start
+		// loop
+        j = 0; H_num = 0;
+        while (H_num <= `H_LEN) begin
+            @(posedge clk);
+            while (j <= `PAT_LEN) begin
+                @(posedge clk);
+                if (out_in_ready) begin
+                #(`PERIOD / 2.0)
+                    in_valid = 1'b1;
+                    inst     = input_data[H_num*15+j][(I_WIDTH*4*2)];
+                    idata_a  = input_data[H_num*15+j][(I_WIDTH*4*2) -1:0];
+                    $display("[Input: %d data, Simulation Cycle: %6d, Time: %11.2f ns",j, cycle_count, `PERIOD*(cycle_count + 2.5));
+                    j = j+1;
+                    //$display("o valid");
+                end else begin
+                #(`PERIOD / 2.0)
+                    in_valid = 1'b0;
+                end
+            end
+            // final
+            @(posedge clk);
+            //#(`PERIOD/2);
+            idata_a  = 0;
+            inst     = 0;
+            in_valid = 1'b0;
+
+            H_num = H_num+1; j = 0; 
+            wait (rst === 1'b1);
+            in_valid =  1'b0;
+            idata_a  = 255'd0;
+            wait (rst === 1'b0);
+        end
+
+    end
+
+    // Output
+    initial begin
+        correct = 0;
+        error   = 0;
+        output_end = 0;
+
+        // reset
+        wait (rst === 1'b1);
+        wait (rst === 1'b0);
+
+        // start
+        @(posedge clk);
+
+        // loop
+        k = 0;
+        while (H_num <= `H_LEN) begin
+            while (k < `PAT_LEN - 4) begin
+                @(negedge clk);
+                if (out_valid) begin
+                    $fdisplay(FILE, "%b", odata); // save output 
+                    $display(
+                        "Test[%d]: Data out=%d %d %d %d ",
+                        k,
+                        odata[11:9], odata[8:6], odata[5:3], odata[2:0]
+                    );
+                    $display("Simulation Cycle: %6d, Time: %11.2f ns", cycle_count, `PERIOD*(cycle_count + 1.5));
+                    k = k+1;
+                end
+                @(posedge clk);
+            end
+            k = 0; 
+        end
+
+        // final
+        output_end = 1;
+    end
+	initial begin
+        cycle_count = 0;
+        wait (rst === 1'b1);
+        wait (rst === 1'b0);
+
+        while (1) begin
+            @(posedge clk);
+            cycle_count = cycle_count + 1;
+        end
+    end
+    // Result
+    initial begin
+        wait (H_num === `H_LEN);
+
+        /*
+        if (error === 0 && H_num === `H_LEN) begin
+            $display("----------------------------------------------");
+            $display("-                 ALL PASS!                  -");
+            $display("----------------------------------------------");
+			
+        end
+        else begin
+            $display("----------------------------------------------");
+            $display("  Wrong! Total Error: %d                      ", error);
+            $display("----------------------------------------------");
+        end
+        */
+
+		$display("Simulation Cycle: %6d, Time: %11.2f ns", cycle_count, `PERIOD*(cycle_count));
+        # (2 * `PERIOD);
+        $fclose(FILE);
+        $finish;
+    end
+
+
+endmodule
+
+
+module clk_gen (
+    output reg clk,
+    output reg rst,
+    output reg rst_n,
+    input wire [31:0] k
+);
+
+    always #(`PERIOD / 2.0) clk = ~clk;
+
+    initial begin
+        clk = 1'b0;
+        rst = 1'b0; rst_n = 1'b1; #(              0.25  * `PERIOD);
+        rst = 1'b1; rst_n = 1'b0; #((`RST_DELAY - 0.25) * `PERIOD);
+        rst = 1'b0; rst_n = 1'b1; 
+    end
+
+    always @* begin
+        if (k === `PAT_LEN - 4) begin
+            rst = 1'b0; rst_n = 1'b1; #(              1.25  * `PERIOD);
+            rst = 1'b1; rst_n = 1'b0; #((`RST_DELAY - 0.25) * `PERIOD);
+            rst = 1'b0; rst_n = 1'b1; 
+        end
+    end
+
+endmodule
